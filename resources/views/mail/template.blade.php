6a3d67aede816bf664f2b48f2cccff4f2a98409b
miftakhul-fitria <miftakhulfitria08@gmail.com>
2020-04-22 19:34:04 +0700

    Sending email in Laravel

---
+++ resources/views/mail/template.blade.php
@@ -0,0 +1,8 @@
+<h3>Writing Mailables</h3>
+<p>All of a mailable class' configuration is done in the build method. Within this method, you may call various methods such as from, subject, view, and attach to configure the email's presentation and delivery.</p>
+<div style="margin-top: 20px;">
+	Regrads,
+	<br/>
+	<br/>
+	<strong>Admin SI Mahasiswa</strong>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -22,24 +22,24 @@ Route::get("/logout","LoginController@logout")->name("logout");
 
 Route::resource("biodata", "BiodataController")->middleware("web");
 
+Route::get("/biodata-mahasiswa", "BiodataController@index")
+->name("biodata.index");
+Route::get("/biodata-mahasiswa/{id}/detail", "BiodataController@show")
+	->name("biodata.show");
+Route::get("/biodata-mahasiswa/create", "BiodataController@create")
+	->name("biodata.create");
+Route::post("/biodata-mahasiswa", "BiodataController@store")
+	->name("biodata.store");
+Route::get("/biodata-mahasiswa/{id}/edit", "BiodataController@edit")
+	->name("biodata.edit");
+Route::post("/biodata-mahasiswa/{id}/update", "BiodataController@update")
+	->name("biodata.update");
+Route::get("/biodata-mahasiswa/{id}/delete", "BiodataController@destroy")
+	->name("biodata.destroy");
+
 //Export excel
 Route::get("/biodata-mahasiswa","BiodataController@index")->name("biodata.index");
 Route::get('/biodata-mahasiswa/export_excel', 'BiodataController@export_excel');
 
-//List mahasiswa
-// Route::middleware("auth")->group(function(){
-	Route::get("/biodata-mahasiswa", "BiodataController@index")
-	->name("biodata.index");
-	Route::get("/biodata-mahasiswa/{id}/detail", "BiodataController@show")
-		->name("biodata.show");
-	Route::get("/biodata-mahasiswa/create", "BiodataController@create")
-		->name("biodata.create");
-	Route::post("/biodata-mahasiswa", "BiodataController@store")
-		->name("biodata.store");
-	Route::get("/biodata-mahasiswa/{id}/edit", "BiodataController@edit")
-		->name("biodata.edit");
-	Route::post("/biodata-mahasiswa/{id}/update", "BiodataController@update")
-		->name("biodata.update");
-	Route::get("/biodata-mahasiswa/{id}/delete", "BiodataController@destroy")
-		->name("biodata.destroy");
-// });
+//Mail
+Route::get("/mail/send", "MailController@send");
